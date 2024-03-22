00fdca2f41b6e5e4b8839dd92f21c1c2fefee37f
JulianoTimmHub <julianohenriquetimm@gmail.com>
2024-03-22 20:26:54 -0300

    corrigindo merge com projeto original do association

--- Gemfile
+++ Gemfile
@@ -6,6 +6,12 @@ gem "rails", "~> 7.1.3"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
+gem 'faker'
+
+gem 'cpf_faker'
+
+gem 'rails_performance'
+
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", "~> 1.4"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,18 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+require 'faker'
+require 'cpf_faker'
+
+Faker::Config.locale = 'pt-BR'
+
 puts "Destroying existing records..."
 User.destroy_all
 Debt.destroy_all
@@ -20,7 +35,7 @@ end
   attrs = {
     name: Faker::Name.name,
     phone_number: Faker::PhoneNumber.phone_number,
-    national_id: CPF.generate,
+    national_id: Faker::CPF.numeric,
     active: [true, false].sample,
     user: User.order('random()').first
   }
@@ -33,4 +48,5 @@ end
       observation: Faker::Lorem.paragraph
     )
   end
+  
 end
